4eab26200abb959413e41d00f1fb0f47febcf183
yairTerrones <yair820100@gmail.com>
2017-11-25 13:57:19 -0600

    Proyecto subido al repositorio.
    El proyecto de compiladores e interpretes B.

---
+++ Proyecto/MapasDeKarnaugh/MapasDeKarnaugh/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace MapasDeKarnaugh
+{
+    partial class Principal
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textVariables = new System.Windows.Forms.RichTextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSimplificar = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groupMapa = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupMapa.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textVariables
+            // 
+            this.textVariables.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textVariables.Location = new System.Drawing.Point(3, 16);
+            this.textVariables.Name = "textVariables";
+            this.textVariables.Size = new System.Drawing.Size(273, 208);
+            this.textVariables.TabIndex = 0;
+            this.textVariables.Text = "";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textVariables);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(279, 227);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = " Ingresa texto: ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 280);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(182, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Función simplificada:";
+            // 
+            // btnSimplificar
+            // 
+            this.btnSimplificar.Location = new System.Drawing.Point(15, 245);
+            this.btnSimplificar.Name = "btnSimplificar";
+            this.btnSimplificar.Size = new System.Drawing.Size(273, 23);
+            this.btnSimplificar.TabIndex = 3;
+            this.btnSimplificar.Text = "Simplificar Función:";
+            this.btnSimplificar.UseVisualStyleBackColor = true;
+            this.btnSimplificar.Click += new System.EventHandler(this.btnSimplificar_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(200, 285);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "label2";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 16);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(405, 208);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // groupMapa
+            // 
+            this.groupMapa.Controls.Add(this.dataGridView1);
+            this.groupMapa.Location = new System.Drawing.Point(297, 12);
+            this.groupMapa.Name = "groupMapa";
+            this.groupMapa.Size = new System.Drawing.Size(411, 227);
+            this.groupMapa.TabIndex = 6;
+            this.groupMapa.TabStop = false;
+            this.groupMapa.Text = " Mapa de Karnaugh ";
+            // 
+            // Principal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(720, 320);
+            this.Controls.Add(this.groupMapa);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnSimplificar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Principal";
+            this.Text = "Mapa de Karnaugh.";
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupMapa.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox textVariables;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSimplificar;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.GroupBox groupMapa;
+    }
+}
+
---
+++ Proyecto/MapasDeKarnaugh/MapasDeKarnaugh/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MapasDeKarnaugh
+{
+    public partial class Principal : Form
+    {
+        public Principal()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSimplificar_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textVariables.Text))//Por mientras. (Aqui va lo que nos entrega la gramatica (Llista)).
+            {
+
+            }
+        }
+
+        /// <summary>
+        /// Método para saber la cantidad de letras que hay en la lista que se genera en el esquema de traducción.
+        /// </summary>
+        /// <param name="l_Variables">Lista generada en el esquema de traducción.</param>
+        /// <returns></returns>
+        public int getCantidadLetras(List<string> l_Variables)
+        {
+            List<char> l_Aux = new List<char>();
+
+            try
+            {
+                foreach (string str in l_Variables)
+                {
+                    for (int i = 0; i < str.Length; i++)
+                    {
+                        if (!l_Aux.Contains(str[i]))
+                            l_Aux.Add(str[i]);
+
+                        if (l_Aux.Count > 4)//Modificar para el proyecto
+                            return -1;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return -1;
+            }
+            return l_Aux.Count;
+        }
+
+
+    }
+}
